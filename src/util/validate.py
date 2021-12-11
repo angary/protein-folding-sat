bdf6331c1868988f75e95bf749c65d68b7b4176d
Gary Sun <garysun0451@gmail.com>
2021-12-11 18:40:48 +1100

    Track the average improvements in variables, clauses, and time

--- src/util/validate.py
+++ src/util/validate.py
@@ -7,6 +7,7 @@ import os
 from src.encode import get_num_vars_and_clauses, encode, solve
 from src.run_tests import get_sequences
 
+MAX_LEN = 20
 INPUT_DIR = "input"
 OUTPUT = "validate.log"
 COMPARE: list[tuple[int, int]] = [
@@ -15,7 +16,13 @@ COMPARE: list[tuple[int, int]] = [
 ]
 
 def main():
-    for sequence in get_sequences(INPUT_DIR, "all", max_len=20):
+    vs = []
+    cs = []
+    ts = []
+    with open(OUTPUT, "w+") as f:
+        pass
+
+    for sequence in get_sequences(INPUT_DIR, "all", max_len=MAX_LEN):
         filename = os.path.join(INPUT_DIR, sequence["filename"])
         results = []
         for dim, version in COMPARE:
@@ -24,14 +31,28 @@ def main():
             v, c = get_num_vars_and_clauses(sequence["filename"], version)
             r = solve(filename, dim, version)
             results += [[v, c, r["max_contacts"], r["duration"]]]
+
+        variable_diff = results[1][0] / results[0][0]
+        clause_diff = results[1][1] / results[0][1]
+        time_diff = results[1][3] / results[0][3] if min(results[1][3], results[0][3]) != 0 else 1
+        vs.append(variable_diff)
+        cs.append(clause_diff)
+        ts.append(time_diff)
+
         result_str = "\n".join(list(map(str,results)))
         with open(OUTPUT, "a") as f:
             f.write(f"{filename}\nVars, Clauses, Max Contacts, Duration\n")
             f.write(f"{result_str}\n")
             f.write(f"Same contacts : {results[0][2] == results[1][2]}\n")
-            f.write(f"Leq variables : {results[0][0] >= results[1][0]}\n")
-            f.write(f"Leq clauses   : {results[0][1] >= results[1][1]}\n")
-            f.write(f"Less time     : {results[0][3] >= results[1][3]}\n\n")
+            f.write(f"Leq variables : {variable_diff <= 1} {variable_diff}\n")
+            f.write(f"Leq clauses   : {clause_diff <= 1} {clause_diff}\n")
+            f.write(f"Less time     : {time_diff < 1} {time_diff}\n\n")
+    with open(OUTPUT, "a") as f:
+        f.write("\n")
+        f.write(f"Avg variables -> {sum(vs) / len(vs)}\n")
+        f.write(f"Avg clauses -> {sum(cs) / len(cs)}\n")
+        f.write(f"Avg time -> {sum(ts) / len(ts)}\n")
+
 
 if __name__ == "__main__":
     main()
